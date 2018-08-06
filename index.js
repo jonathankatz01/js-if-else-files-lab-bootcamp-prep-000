72e89a94fb2e1ebb4bd2ac0b25fed3406117f89f
jonathank1@ufl.edu <jonathank1@ufl.edu>
2018-08-06 21:29:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var name = "susan";
-var height = "74";
+var name = "susan"
+var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 }
-
 else {
   console.log("Expected the name to be susan")
 }
